af5220260381e1e6b5e12ba4350b3f5fd463fc32
emirhanusta <emirhan1usta@gmail.com>
2022-11-09 15:23:15 +0300

    hafta 3 ödev 8

---
+++ PatikaDev/DeseneGoreMetodOlusturma/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	static int n;
+	static boolean b=true;
+	
+	public static int pattern(int number) { //
+		if (number>0 && b) {
+			System.out.println(number);
+			return pattern(number-5);
+		}else if (n>=number) {
+			b=false;
+			System.out.println(number);
+			return pattern(number+5);
+		}else return 1;
+	}
+	
+	public static void main(String [] args) {
+		Scanner scanner = new Scanner(System.in); 
+		System.out.println("Enter the number");
+		int number = scanner.nextInt();
+		n=number;
+
+		pattern(number);
+	}
+}
