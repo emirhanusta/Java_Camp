ec7d4c806e29409b5c0ef69a94ffbfd7b2f4db07
emirhanusta <emirhan1usta@gmail.com>
2022-11-12 20:53:26 +0300

    added

--- PatikaDev/PalindromikKelimeBulma/src/Main.java
+++ PatikaDev/PalindromikKelimeBulma/src/Main.java
@@ -18,6 +18,7 @@ public class Main {
     	System.out.println("Enter a word");
     	String word = scanner.nextLine();
         System.out.println(isPalindrome(word));
+        scanner.close();
         
     }
 
