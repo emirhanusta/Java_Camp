cae775b8c087e8b3e6095b7f44c9b6a2cf4c07b6
emirhanusta <emirhan1usta@gmail.com>
2022-11-13 20:07:20 +0300

    updated

--- PatikaDev/MaasHesaplayici/src/Employee.java
+++ PatikaDev/MaasHesaplayici/src/Employee.java
@@ -15,31 +15,31 @@ public class Employee {
 		this.hireYear = hireYear;
 	}
 	
-	public double tax(double salary) {
-		if (salary>1000) {
+	public double tax(double salary) {	// method for calculating tax
+		if (salary>1000) { 				// if salary is greater than 1000
 			return salary*0.03; 
 		}
 		return 0;
 	}
 	
-	public double bonus(int workHours) {
-		if(workHours>40) {
+	public double bonus(int workHours) { 		// method for calculating bonus
+		if(workHours>40) {					// if work hours are greater than 40
 			return (workHours-40)*30;
 		}
 		return 0;
 	}
 	
-	public double raiseSalary(double salary,int hireYear) {
-		if (2021-hireYear<10) {
+	public double raiseSalary(double salary,int hireYear) {	// method for calculating raise salary
+		if (2021-hireYear<10) {							// if hire year is less than 10
 			return (salary/100)*5;
 		}
-		else if(2021-hireYear>19) {
+		else if(2021-hireYear>19) {					// if hire year is greater than 19
 			return (salary/100)*15;
-		}else
+		}else										// if hire year is between 10 and 19
 			return (salary/100)*10;
 	}
 	
-	public String toString(Employee employee) {
+	public String toString(Employee employee) {	// method for printing employee information
 		double tax=employee.tax(employee.salary);
 		double bonus =employee.bonus(employee.workHours);
 		double raiseSalary =employee.raiseSalary(employee.salary,employee.hireYear);
--- PatikaDev/MaasHesaplayici/src/Main.java
+++ PatikaDev/MaasHesaplayici/src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Employee employee =new Employee("kemal", 2000, 45, 1985);
+		Employee employee =new Employee("kemal", 2000, 45, 1985);	// creating employee object
 
-		System.out.println(employee.toString(employee));
+		System.out.println(employee.toString(employee));	// printing employee information
 
 	}
 
