f01a8efe2db5214785bbebca85b40bc14989ded6
emirhanusta <emirhan1usta@gmail.com>
2022-11-13 20:48:25 +0300

    added

--- PatikaDev/TerkarEdenElemanlar/src/Main.java
+++ PatikaDev/TerkarEdenElemanlar/src/Main.java
@@ -6,24 +6,25 @@ public class Main {
 		int[] array = {10, 20, 20, 10, 10, 20, 5, 20};
 		int[] count = new int[array.length];
 		int temp = 0;
-		for (int i = 0; i < array.length; i++) {
+
+		for (int i = 0; i < array.length; i++) {	// Loop to find how many times elements are repeated
 			temp = 1;
-			for (int j = i + 1; j < array.length; j++) {
-				if (array[i] == array[j]) {
+			for (int j = i + 1; j < array.length; j++) {	// Loop to find how many times elements are repeated
+				if (array[i] == array[j]) {	// If elements are equal, increase temp
 					temp++;
 					count[j] = -1;
 				}
 			}
-			if (count[i] != -1) {
+			if (count[i] != -1) {	// If elements are not equal, increase count
 				count[i] = temp;
 			}
 		}
-		for (int i = 0; i < count.length; i++) {
-			if (count[i] != -1) {
+		for (int i = 0; i < count.length; i++) {	// Loop to print how many times elements are repeated
+			if (count[i] != -1) {	// If elements are not equal, print elements and how many times they are repeated
 				System.out.println(array[i] + " repeated " + count[i] + " times");
 			}
 		}
-		
+
 	
 
 	}
